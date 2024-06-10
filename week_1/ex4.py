3bb5e65a2e33d6a3dbb4f77f7327a20b1f9f0183
ann <minhanh.dang0612@gmail.com>
2024-06-11 01:12:40 +0700

    excercise(m01ex01-subex01): add missed code blocks

--- week_1/ex4.py
+++ week_1/ex4.py
@@ -1,4 +1,5 @@
 import math
+
 def check_n(n):
   if isinstance(n, int) == 0 and n > 0:
     return True
@@ -8,13 +9,27 @@ def factorial(n):
     return 1
   else:
     return n * factorial(n-1)
-  
-print(factorial(8))
-print(math.factorial(8))
 
-def approx_cos (x , n ) :
+def approx_cos (x , n) :
   sum = 0
-  
   for n in range(0, n):
     sum = sum + pow((-1),n) * pow(x, 2 * n) / factorial(2 * n)
+  return sum
+
+def approx_sin (x , n) :
+  sum = 0
+  for n in range(0, n):
+    sum += pow((-1),n) * pow(x, 2 * n + 1) / factorial(2 * n + 1)
+  return sum
+
+def approx_sinh (x , n) :
+  sum = 0
+  for n in range(0, n):
+    sum += pow(x, 2 * n + 1) / math.factorial(2 * n + 1)
+  return sum
+
+def approx_cosh (x , n) :
+  sum = 0
+  for n in range(0, n):
+    sum += pow(x, 2 * n) / math.factorial(2 * n)
   return sum
